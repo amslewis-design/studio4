871afeea7eff28407d88fa445cea991a0f4aead8
amslewis-design <amslewis@googlemail.com>
2026-01-12 16:57:18 +0000

    blog 404 fix attempt 1

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import fect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import Link from 'next/link';
 import { useParams } from 'next/navigation';
 import { motion } from 'framer-motion';
