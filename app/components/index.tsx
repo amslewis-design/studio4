d0ac10b53043dfc33d5e1815d9220b412e090e5a
amslewis-design <amslewis@googlemail.com>
2026-01-25 00:27:57 +0000

    navbar fix-2

--- app/components/index.tsx
+++ app/components/index.tsx
@@ -1,7 +1,6 @@
 'use client';
 
 import React, { useEffect, useMemo, useState } from "react";
-import Image from "next/image";
 import Link from "next/link";
 import { useTranslations } from 'next-intl';
 import { AnimatePresence, motion } from "framer-motion";
